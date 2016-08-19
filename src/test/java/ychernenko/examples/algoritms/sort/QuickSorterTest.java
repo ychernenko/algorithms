8a82ba6d627b1cf5717f86539b279eb2691d7eb8
ychernenko <yura4e@gmail.com>
2016-08-19 14:30:22 +0300

    Minor test changes

--- src/test/java/ychernenko/examples/algoritms/sort/QuickSorterTest.java
+++ src/test/java/ychernenko/examples/algoritms/sort/QuickSorterTest.java
@@ -1,11 +1,9 @@
 package ychernenko.examples.algoritms.sort;
 
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -14,10 +12,9 @@ public class QuickSorterTest {
 
     @Test
     public void testSort() {
-        int size = 100;
-        List<Integer> items = Stream.generate(() -> (int)(Math.random() * size)).limit(size).collect(Collectors.toList());
-        List<Integer> expected = new ArrayList<>(items);
-        Collections.sort(expected);
+        int size = 1000;
+        List<Integer> items = Stream.generate(() -> (int)(Math.random() * size)).limit(size).collect(toList());
+        List<Integer> expected = items.stream().sorted().collect(toList());
         List<Integer> actual = new QuickSorter<Integer>().sort(items);
         assertEquals(expected, actual);
     }
